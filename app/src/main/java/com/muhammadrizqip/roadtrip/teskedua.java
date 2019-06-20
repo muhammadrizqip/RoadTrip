3ab4270caf39e2ca88701b31c262a63f4ef4a106
Muhammad Rizqi Perdana <muhammadrizqip@gmail.com>
2019-06-20 19:45:30 +0700

    dwik dah selse nih

---
+++ app/src/main/java/com/muhammadrizqip/roadtrip/teskedua.java
@@ -0,0 +1,14 @@
+package com.muhammadrizqip.roadtrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class teskedua extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teskedua);
+    }
+}
